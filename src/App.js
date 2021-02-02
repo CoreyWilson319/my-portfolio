a259d1f0cfee21bcd49eec0c21f8e96bfd5ff1f3
Your Name <you@example.com>
2021-02-01 22:27:16 -0500

    More templating

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import { BrowserRouter, Switch, Route, Link, NavLink } from "react-router-dom"
-import logo from './logo.svg';
-import './App.css';
+import Resume from "./resources/Resume.pdf"
 import About from "./components/about"
 import Home from "./components/home"
+import Projects from "./components/projects"
 
 
 function App() {
   return (
     <BrowserRouter>
+    <nav>
+      <NavLink to="/">Home</NavLink>|
+      <NavLink to="/about">About</NavLink>|
+      <NavLink to="/projects">Projects</NavLink>
+    </nav>
       <Route exact path="/" component={Home} />
       <Route exact path="/about" component={About} />
+      <Route exact path="/projects" component={Projects} />
+      <a href={Resume}>Resume</a>
     </BrowserRouter>
     
   );
---
+++ src/components/projects.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom"
+
+function Projects () {
+    return(
+        <div>
+            <h1 className="page_title">Projects</h1>
+            <p>Cleo's Site (Need to finish)</p>
+            <p>Mental Health App (Need to start)</p>
+            <p>My own twist on the budgeting app we made</p>
+        </div>
+    )
+}
+
+export default Projects
