bc7f28dea75fc1d0585c3d376b57e550d1506ecb
Your Name <you@example.com>
2021-02-01 20:37:54 -0500

    some work

---
+++ src/components/home.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom"
+
+function Home () {
+    return(
+        <div>
+            <h1 className="page_title">Home</h1>
+            <p>Brand Statement</p>
+        </div>
+    )
+}
+
+export default Home
