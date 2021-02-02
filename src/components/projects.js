09407ae09250c50fd44700edd6baef1ae3668a68
Your Name <you@example.com>
2021-02-02 17:54:17 -0500

    added deployment links

--- src/components/projects.js
+++ src/components/projects.js
@@ -9,6 +9,10 @@ function Projects () {
             <p>Mental Health App (Need to start)</p>
             <p>My own twist on the budgeting app we made</p>
             <p>All GA projects</p>
+            <p><a href="https://set-aside.herokuapp.com/" target="_blank">Set Aside</a></p>
+            <p><a href="https://pokemongofindthem.herokuapp.com/" target="_blank">Pokemon Go Find Them</a></p>
+            <p><a href="https://gametrackersei.herokuapp.com/" target="_blank">Game Tracker</a></p>
+            <p><a href="https://coreywilson319.github.io/" target="_blank">Blasterboy</a></p>
             <p>Show screenshots on Portfolio</p>
             <p>Images of projects and explain the project</p>
         </div>
